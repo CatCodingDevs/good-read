b5449dc134734af90efbd2a6a571dd77916aa658
AbdullayevAbbos <abdullayevabbos534@gmail.com>
2023-02-20 17:13:36 +0500

    dbcontext implemented

---
+++ src/GoodRead.DataAccess/DbContexts/AppDbContext.cs
@@ -0,0 +1,36 @@
+﻿using GoodRead.Domain.Entities.Books;
+using GoodRead.Domain.Entities.Orders;
+using GoodRead.Domain.Entities.Users;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GoodRead.DataAccess.DbContexts
+{
+    public class AppDbContext : DbContext
+    {
+        public AppDbContext(DbContextOptions<AppDbContext> options) : base(options)
+        {
+        }
+        
+        public virtual DbSet<User> Users { get; set; } 
+        public virtual DbSet<Book> Books { get; set; }
+        public virtual DbSet<Author> Authors { get; set; }
+        public virtual DbSet<Genre> Genres { get; set; }
+        public virtual DbSet<Publisher> Publishers { get; set; }
+        public virtual DbSet<Category> Categories { get; set; }
+        public virtual DbSet<Order> Orders { get; set; }
+        public virtual DbSet<OrderDetail> OrderDetails { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<User>().HasIndex(x => x.Email).IsUnique(true);
+            modelBuilder.Entity<Genre>().HasIndex(x => x.Name).IsUnique(true);
+            modelBuilder.Entity<Category>().HasIndex(x => x.CategoryName).IsUnique(true);
+            modelBuilder.Entity<Publisher>().HasIndex(x => x.Name).IsUnique(true);  
+        }
+    }
+}
