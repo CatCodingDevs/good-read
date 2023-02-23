5a3fce372186c4bf675256820df442f8fa9fa2bb
AbdullayevAbbos <abdullayevabbos534@gmail.com>
2023-02-23 16:13:43 +0500

    updated

---
+++ src/GoodRead.Service/DTOs/Books/GenreCreateDto.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GoodRead.Service.DTOs.Books
+{
+    public class GenreCreateDto
+    {
+        public string Name { get; set; } = string.Empty;
+    }
+}
