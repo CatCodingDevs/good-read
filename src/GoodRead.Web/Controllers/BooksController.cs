8fdf4987942379e39f53243fd2e68a45aa236a58
shakhriyor akhadov <shakhriyor.akhadov@gmail.com>
2023-02-26 17:52:17 +0500

    some changes

---
+++ src/GoodRead.Web/Controllers/BooksController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace GoodRead.Web.Controllers;
+
+[Route("books")]
+public class BooksController : Controller
+{
+    public IActionResult Index()
+    {
+        return View();
+    }
+}
