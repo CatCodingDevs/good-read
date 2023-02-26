1c310c32cba873475fda9d6c15fa602b9732ea2e
shakhriyor akhadov <shakhriyor.akhadov@gmail.com>
2023-02-26 17:52:17 +0500

    some changes

---
+++ src/GoodRead.Service/Interfaces/Addresses/IAddressService.cs
@@ -0,0 +1,17 @@
+﻿using GoodRead.Domain.Entities.Addresses;
+using GoodRead.Service.DTOs.Addresses;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GoodRead.Service.Interfaces.Addresses
+{
+    public interface IAddressService
+    {
+        Task<bool> CreateAsync(AddressCreateDto addressCreateDto);
+        Task<bool> UpdateAsync(AddressCreateDto addressCreateDto);
+        Task<bool> DeleteAsync(long id);
+    }
+}
