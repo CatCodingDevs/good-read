179d8cbcfee4450ebe04a147cd4a282fea92cead
AbdullayevAbbos <abdullayevabbos534@gmail.com>
2023-02-28 14:02:29 +0500

    jwt configured

--- src/GoodRead.Service/Services/Common/AuthManager.cs
+++ src/GoodRead.Service/Services/Common/AuthManager.cs
@@ -30,9 +30,9 @@ namespace GoodRead.Service.Services.Common
             var securityKey = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(_configuration["Key"]));
             var credentials = new SigningCredentials(securityKey, SecurityAlgorithms.HmacSha256Signature);
             var tokenDescription = new JwtSecurityToken(_configuration["Issuer"], _configuration["Audience"], claims,
-                expires: DateTime.Now.AddMinutes(double.Parse(_configuration["Lifetime"])),
+                expires: DateTime.Now.AddDays(double.Parse(_configuration["Lifetime"])),
                 signingCredentials: credentials);
-
+                
             return new JwtSecurityTokenHandler().WriteToken(tokenDescription);
         }
     }
