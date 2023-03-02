fba9e3c0b30e0cfb9a3b2f24efe074642afcd771
Aslonboyev <temuraslonboyev@gmail.com>
2023-03-02 23:12:35 +0500

    Upd

--- src/GoodRead.Service/Services/Users/AccountService.cs
+++ src/GoodRead.Service/Services/Users/AccountService.cs
@@ -1,9 +1,12 @@
-﻿using GoodRead.Service.DTOs.Accounts;
+﻿using GoodRead.DataAccess.Exceptions;
+using GoodRead.DataAccess.Interfaces;
+using GoodRead.Service.DTOs.Accounts;
 using GoodRead.Service.DTOs.Users;
 using GoodRead.Service.Interfaces.Common;
 using GoodRead.Service.Interfaces.Managments;
 using GoodRead.Service.Interfaces.Users;
 using GoodRead.Service.Services.Common;
+using Microsoft.AspNetCore.Http.HttpResults;
 
 namespace GoodRead.Service.Services.Users
 {
@@ -11,16 +14,22 @@ namespace GoodRead.Service.Services.Users
     {
         private readonly IAuthManager authManager;
         private readonly IIdentityHelperService identity;
+        private readonly IUnitOfWork unit;
 
-        public AccountService(IAuthManager authManager, IIdentityHelperService identity)
+        public AccountService(IAuthManager authManager, IIdentityHelperService identity, IUnitOfWork unit)
         {
             this.authManager = authManager;
             this.identity = identity;
+            this.unit = unit;
         }
 
-        public Task<string> LogInAsync(AccountLoginDto accountLogin)
+        public async Task<string> LogInAsync(AccountLoginDto accountLogin)
         {
-            throw new NotImplementedException();
+            var result = await unit.UserRepository.GetAsync(x => x.Email == accountLogin.Email);
+            if (result is null)
+            {
+                return StatusCodeException()
+            }
         }
 
         public Task<bool> RegisterAsync(AccountRegisterDto accountCreate)
