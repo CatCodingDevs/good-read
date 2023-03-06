5531e502250e9c9b088708925b4a01d525360d90
akhadov <shakhriyor.akhadov@gmail.com>
2023-03-06 13:44:44 +0500

    fixed layout

---
+++ src/GoodRead.Service/Interfaces/Books/IBookService.cs
@@ -0,0 +1,9 @@
+﻿using GoodRead.Service.DTOs.Books;
+
+namespace GoodRead.Service.Interfaces.Books
+{
+    public interface IBookService
+    {
+        Task<bool> CreateAsync(BookCreateDto bookCreateDto);
+    }
+}
