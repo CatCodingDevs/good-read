93ac9b138cbc0535265ae7603955ac312c68285e
shakhriyor akhadov <shakhriyor.akhadov@gmail.com>
2023-07-11 11:37:44 +0500

    add comment

--- src/GoodRead.Domain/Entities/Books/Genre.cs
+++ src/GoodRead.Domain/Entities/Books/Genre.cs
@@ -4,6 +4,9 @@ namespace GoodRead.Domain.Entities.Books
 {
     public class Genre : Auditable
     {
+        /// <summary>
+        /// Gets or Sets Genre name
+        /// </summary>
         public string Name { get; set; } = String.Empty;
     }
 }
